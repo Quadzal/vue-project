cfd448014bc890fa3df1ba98222de0552875a262
Quadzal <40356538+Quadzal@users.noreply.github.com>
2019-08-03 19:44:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
+
+fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
 
 app.use(express.static(path.join(__dirname, '/dist')))
 app.get("/",(req, res) => {
