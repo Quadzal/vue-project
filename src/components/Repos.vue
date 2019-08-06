dc07222fc3eb53e17cf2aabf573b383b7b7fce8d
Quadzal <40356538+Quadzal@users.noreply.github.com>
2019-08-06 17:38:07 +0300

    Update Repos.vue

--- src/components/Repos.vue
+++ src/components/Repos.vue
@@ -1,12 +1,14 @@
 <template>
     <div v-if="repos.length">
         <h3 class="page-heading mb-3">En son repolar</h3>
-        <div id="repos" style ="margin-bottom: 100px;" v-for="repo in repos">
+        <div id="repos" style ="margin-bottom: 20px;" v-for="repo in repos" :key="repo">
             <div class="card card-body mb-2">
                 <div class="row">
                     <div class="col-md-6">
-                        <span>{{repo.name}}</span> 
-                        <a :href="repo.html_url" target = "_blank" class = "btn btn-danger"> Repoya Git</a>
+                        <span>{{repo.name}}</span>
+                        <span class="badge badge-danger">
+                            <a :href="repo.html_url" target = "_blank" class="text-white"> Repoya Git</a>
+                            </span>
                     </div>
                     <div class="col-md-6">
                         <span class="badge badge-primary">Yıldızlar: {{repo.stargazers_count}} </span>
